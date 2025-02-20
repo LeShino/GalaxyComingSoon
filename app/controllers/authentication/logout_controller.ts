3ef724098bc0774c33c6172cbcccbdd12011da76
Laubet Guy-Gael <laubet@Laubet.local>
2025-02-20 18:47:07 +0000

    MaJ Authentication Final (Bug)

--- app/controllers/authentication/logout_controller.ts
+++
@@ -1,9 +0,0 @@
-import type { HttpContext } from '@adonisjs/core/http'
-
-export default class LogoutController {
-  
-  async show({}: HttpContext) {}
-  
-  async store({}: HttpContext) {}
-  
-}
--- start/routes.ts
+++ start/routes.ts
@@ -8,7 +8,9 @@
 */
 
 
-import RegisterController from '#controllers/authentication/register_controller'
+import LoginController from '#controllers/auth/login_controller'
+import LogoutController from '#controllers/auth/logout_controller'
+import RegisterController from '#controllers/auth/register_controller'
 import EmailController from '#controllers/email_controller'
 import router from '@adonisjs/core/services/router'
 
@@ -19,4 +21,16 @@ router.post('/subscribe', [EmailController, 'store']).as('suscribe.store')
 router.group(() => {
     router.get('/register', [RegisterController, 'show']).as('register.show')
     router.post('/register', [RegisterController, 'store']).as('register.store')
+
+    router.get('/login', [LoginController, 'show']).as('login.show')
+    router.post('/login', [LoginController, 'store']).as('login.store')
+
+    router.post('/logout', [LogoutController, 'handle']).as('logout')
 }).as('auth')
+
+// router.on('/panel').render('pages/admin/accueil')
+router.get('/panel', async (ctx) => {
+    await ctx.auth.check
+    return ctx.view.render('pages/admin/panel')
+})
+
